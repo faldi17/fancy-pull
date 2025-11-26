a79e1b0d150b784366ce56afffe2cd1b04d9d556
Faldi <faldiansyahk@gmail.com>
2025-11-26 21:44:21 +0700

    liquid refresh success

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -4,7 +4,10 @@ import 'package:liquid_pull_to_refresh/liquid_pull_to_refresh.dart';
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  Future<void> _handleRefresh() async {}
+  Future<void> _handleRefresh() async {
+    // reloading takes some time
+    return await Future.delayed(Duration(seconds: 2));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,6 +15,8 @@ class HomePage extends StatelessWidget {
       backgroundColor: Colors.lightBlue[400],
       body: LiquidPullToRefresh(
         onRefresh: _handleRefresh,
+        color: Colors.lightBlue[200],
+        height: 300,
         child: ListView(
           children: [
             Padding(
