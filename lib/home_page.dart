7ca6bc3ff20def3aa5ad98f2f9efe56b58b552f5
Faldi <faldiansyahk@gmail.com>
2025-11-26 14:33:41 +0700

    animation success

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,22 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:liquid_pull_to_refresh/liquid_pull_to_refresh.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
+
+  Future<void> _handleRefresh() async {}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlue[400],
-      body: ListView(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(25.0),
-            child: ClipRRect(
-              borderRadius: BorderRadiusGeometry.circular(20),
-              child: Container(height: 300, color: Colors.lightBlue[600]),
+      body: LiquidPullToRefresh(
+        onRefresh: _handleRefresh,
+        child: ListView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: ClipRRect(
+                borderRadius: BorderRadiusGeometry.circular(20),
+                child: Container(height: 300, color: Colors.lightBlue[600]),
+              ),
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: ClipRRect(
+                borderRadius: BorderRadiusGeometry.circular(20),
+                child: Container(height: 300, color: Colors.lightBlue[800]),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: ClipRRect(
+                borderRadius: BorderRadiusGeometry.circular(20),
+                child: Container(height: 300, color: Colors.lightBlue[900]),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
